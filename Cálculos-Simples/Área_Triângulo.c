f84834296be0834921a7da85d2fee2fad3b8ca70
Gabriel Torres Machado <gabrieltmachado87@gmail.com>
2025-07-27 15:15:59 -0300

    Cálculo da área de um triângulo

---
+++ Cálculos-Simples/Área_Triângulo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int b, h, a;
+    printf("Digite a base e altura do triângulo: ");
+    scanf("%d %d", &b, &h);
+    a=(b*h)/2;
+    printf("%d", a);
+
+return 0;
+}
